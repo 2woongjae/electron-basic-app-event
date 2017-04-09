f105f5b87675ad7d3fe2088cc527d5822997c5a9
Mark Lee <mark@protopie.io>
2017-04-09 16:49:01 +0900

    한글 주석으로 변경

--- main.js
+++ main.js
@@ -6,19 +6,18 @@ console.log('start');
 
 app.on('will-finish-launching', () => {
     /*
-     the application has finished basic startup.
      [Windows / Linux] => = ready
      [macOS] => = applicationWillFinishLaunching (NSApplication)
-     Setup 'open-file', 'open-url' events
-     Start 'crash reporter' and 'auto updater'
-     In most cases, you should just do everything in the ready event handler.
+
+     'open-file', 'open-url' 이벤트에 대해 설정하거나 (둘다 macOS 용 이벤트)
+     'crash reporter' and 'auto updater' 를 시작합니다.
     */
     console.log('will-finish-launching');
 });
 
 app.on('ready', (launchInfo) => {
     /*
-     Electron has finished initializing.
+     일렉트론 앱이 초기화되어 준비된 상태입니다.
      [macOS] launchInfo Object
      */
     console.log(`ready : ${JSON.stringify(launchInfo)}`);
@@ -33,7 +32,7 @@ app.on('ready', (launchInfo) => {
 
 app.on('window-all-closed', () => {
     /*
-     all windows have been closed.
+     모든 윈도우가 닫히자 마자 불립니다.
      */
     console.log('window-all-closed');
     app.quit();
@@ -41,7 +40,8 @@ app.on('window-all-closed', () => {
 
 app.on('before-quit', (event) => {
     /*
-     starts closing its windows.
+     앱 종료가 시작되어서 앱의 모든 윈도우들을 클로즈 하기 시작할때 불립니다.
+     이미 닫혀있어도 불립니다.
      */
     // event.preventDefault();
     console.log('before-quit');
@@ -49,7 +49,7 @@ app.on('before-quit', (event) => {
 
 app.on('will-quit', (event) => {
     /*
-     all windows have been closed and the application will quit.
+     모든 윈도우가 닫히고 나서 메인 앱 프로세스를 종료하기 직전에 불립니다.
      */
     // event.preventDefault();
     console.log('will-quit');
@@ -57,19 +57,29 @@ app.on('will-quit', (event) => {
 
 /*
  window-all-closed 와 will-quit 차이
- If you do not subscribe to this event and all windows are closed, the default behavior is to quit the app;
- however, if you subscribe, you control whether the app quits or not.
- If the user pressed Cmd + Q, or the developer called app.quit(), Electron will first try to close all the windows and then emit the will-quit event, and in this case the window-all-closed event would not be emitted.
+ 만약에 window-all-closed 이벤트를 받는 함수를 설정하지 않으면, 모든 윈도우들이 닫혔을때 app.quit() (before-quit => will-quit => quit) 이 진행됩니다.
+ 만약에 window-all-closed 이벤트를 받는 함수를 설정 한다면, app.quit() 을 할지 안할지 결정할 수 있습니다.
+
+ 모든 윈도우를 닫아서가 아니라, <Cmd + Q> 를 호출하거나, 코드에서 직접 app.quit() 을 호출해서 앱을 종료하면,
+ 모든 윈도우를 닫은 후 will-quit 이벤트를 발생시킨다.
+ 이럴 경우에는 window-all-closed 이벤트는 발생하지 않는다.
  */
 
 app.on('quit', (event, exitCode) => {
+    /*
+     최종적으로 종료되면서 불립니다.
+     */
     console.log(`quit : ${exitCode}`);
 });
 
 /*
     [macOS]
-    열려있는 윈도우가 없고 dock 에서 어플리케이션 아이콘을 클릭하면 발생하는 이벤트
+    dock 에서 어플리케이션 아이콘을 클릭하면 발생하는 이벤트
  */
 app.on('activate', (event, hasVisibleWindows) => {
+    /*
+     열려있는 윈도우가 있으면, hasVisibleWindows 가 true 입니다.
+     없으면, hasVisibleWindows 가 false 입니다.
+     */
     console.log(`activate : ${hasVisibleWindows}`);
 });
